cf9d9c64ba24724057d34056e882eed8bdce9876
shahensha <64788000+mr-shahensha@users.noreply.github.com>
2021-05-20 18:48:50 +0530

    ok

--- game.js
+++ game.js
@@ -6,6 +6,7 @@ let accept=false;
 let score=0;
 let questioncounter=0;
 let avq=[];
+const scre=document.getElementById("scre");
 
 let questions=[
     {
@@ -16,50 +17,50 @@ let questions=[
         choice4:"Barack Obama",
         answer:2
     },
-    {
-        question:"origin country of coronavirus ?",
-        choice1:"China",
-        choice2:"Russia",
-        choice3:"United States	",
-        choice4:"india",
-        answer:1
-    },
+    // {
+    //     question:"origin country of coronavirus ?",
+    //     choice1:"China",
+    //     choice2:"Russia",
+    //     choice3:"United States	",
+    //     choice4:"india",
+    //     answer:1
+    // },
 
-    {
-        question:"what is the full form of html ?",
-        choice1:"hyper text markup language",
-        choice2:"hyper text mark language",
-        choice3:"hyper tune markup lol",
-        choice4:"high text make language",
-        answer:1
-    },
+    // {
+    //     question:"what is the full form of html ?",
+    //     choice1:"hyper text markup language",
+    //     choice2:"hyper text mark language",
+    //     choice3:"hyper tune markup lol",
+    //     choice4:"high text make language",
+    //     answer:1
+    // },
 
-    {
-        question:"who created the social sites facebook ?",
-        choice1:"bill gates",
-        choice2:"Mark Zuckerberg",
-        choice3:"elon musk",
-        choice4:"jeff bezos",
-        answer:2
-    },
+    // {
+    //     question:"who created the social sites facebook ?",
+    //     choice1:"bill gates",
+    //     choice2:"Mark Zuckerberg",
+    //     choice3:"elon musk",
+    //     choice4:"jeff bezos",
+    //     answer:2
+    // },
 
-    {
-        question:"who is the lead actor of 3idiots ?",
-        choice1:"Boman Irani",
-        choice2:"Kareena Kapoor",
-        choice3:"R. Madhavan",
-        choice4:"Aamir Khan",
-        answer:4
-    },
+    // {
+    //     question:"who is the lead actor of 3idiots ?",
+    //     choice1:"Boman Irani",
+    //     choice2:"Kareena Kapoor",
+    //     choice3:"R. Madhavan",
+    //     choice4:"Aamir Khan",
+    //     answer:4
+    // },
 
-    {
-        question:"the height of mount everest",
-        choice1:"8,894 m",
-        choice2:"8,984 m",
-        choice3:"8,849 m",
-        choice4:"8,489 m",
-        answer:3
-    },
+    // {
+    //     question:"the height of mount everest",
+    //     choice1:"8,894 m",
+    //     choice2:"8,984 m",
+    //     choice3:"8,849 m",
+    //     choice4:"8,489 m",
+    //     answer:3
+    // },
 ];
 
 const CURRECT_ANS=10;
@@ -112,7 +113,8 @@ choices.forEach(choice=>{
 
 
         if(avq.length<1){
-            document.getElementById("qs").style.display="none"; 
+            document.getElementById("qs").style.display="none";
+            scre.classList.add("scre1");
         }
         else{
             document.getElementById("qs").style.display="inline"; 
@@ -126,3 +128,4 @@ incrementScore=num=>{
 }
 
 startGame();
+
